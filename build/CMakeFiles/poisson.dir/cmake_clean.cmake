6664a355e7631df70fc82930fbed2560e23b5aee
Jon Vegard Venås <jonvegar@markov.math.ntnu.no>
2016-04-09 16:22:44 +0200

    for morten2

---
+++ build/CMakeFiles/poisson.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/poisson.dir/poisson.c.o"
+  "poisson.pdb"
+  "poisson"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/poisson.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
