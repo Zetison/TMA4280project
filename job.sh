104e8da61431fd1031b2af9353b2876976c16c05
Jon Vegard Venås <jonvegar@service0.vilje.hpc.ntnu.no>
2016-04-14 19:48:23 +0200

    minorChanges

--- job.sh
+++ job.sh
@@ -13,6 +13,7 @@ cd $PBS_O_WORKDIR
 ######################################################################################
 echo "Exercise 2 - convergence plot"
 
+echo $(date)
 k_start=3
 k_end=16
 rhsType=1
@@ -20,7 +21,7 @@ P=36
 for t in 4 8; do
 	for k in `seq $k_start $k_end`; do
 		printf "t = "$t", k = "$k"\n"
-		mpiexec_mpt -np $P omplace -nt $t -vv ./build/poisson $k $rhsType 0 1 
+		mpiexec_mpt -np $P  omplace -nt $t -c 0-15:bs=$t+st=8 -vv ./build/poisson $k $rhsType 0 1 
 	done
 done
 
@@ -29,19 +30,21 @@ done
 ######################################################################################
 echo "Exercise 2"
 
+echo $(date)
 rhsType=1
 k=12
 
 for P in 1 2 4 8 16 32; do
 	for t in 1 2 4 8; do
 		printf "P = "$P", t = "$t"\n"
-		mpiexec_mpt -np $P omplace -nt $t -vv ./build/poisson $k $rhsType 0 0 
+		mpiexec_mpt -np $P  omplace -nt $t -c 0-15:bs=$t+st=8 -vv ./build/poisson $k $rhsType 0 0 
 	done
 done
 
 ######################################################################################
 echo "Exercise 3"
 
+echo $(date)
 rhsType=1
 k=14
 arr=(1 2 3 4 6 9 12 18 36)
@@ -50,12 +53,13 @@ for i in `seq 5 $N_arr`; do
 	P=${arr[$[i-1]]}
 	t=${arr[$[N_arr-i]]}
 	printf "P = "$P", t = "$t"\n"
-	mpiexec_mpt -np $P omplace -nt $t -vv ./build/poisson $k $rhsType 0 0 
+	mpiexec_mpt -np $P  omplace -nt $t -c 0-15:bs=$t+st=8 -vv ./build/poisson $k $rhsType 0 0 
 done
 
 ######################################################################################
 echo "Exercise 4"
 
+echo $(date)
 rhsType=1
 k_start=10
 k_end=14
@@ -64,13 +68,14 @@ t=8
 for P in 1 2 4 8 16 32; do
 	for k in `seq $k_start $k_end`; do
 		printf "P = "$P", k = "$k"\n"	
-		mpiexec_mpt -np $P omplace -nt $t -vv ./build/poisson $k $rhsType 0 0 
+		mpiexec_mpt -np $P  omplace -nt $t -c 0-15:bs=$t+st=8 -vv ./build/poisson $k $rhsType 0 0 
 	done
 done
 
 ######################################################################################
 echo "Exercise 5"
 
+echo $(date)
 mkdir -p postProcessing
 rhsType=3
 k=11
@@ -78,5 +83,6 @@ t=8
 P=36
 
 cd build
-mpiexec_mpt -np $P omplace -nt $t -vv ./poisson $k $rhsType 1 0
+mpiexec_mpt -np $P  omplace -nt $t -c 0-15:bs=$t+st=8 -vv ./poisson $k $rhsType 1 0
 
+echo $(date)
